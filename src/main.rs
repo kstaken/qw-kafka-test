0e41185859ff0a33b118587372212c471351be00
Kimbro Staken <kstaken@terascope.io>
2022-07-12 14:08:55 -0700

    Consistent data writing and graceful shutdown handling

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::fs;
 use std::io::ErrorKind;
 use std::sync::{Arc, Mutex};
+use chrono;
+
+use tokio::signal;
+use tokio::sync::{mpsc, broadcast};
 
 use clap::{App, Arg};
-use log::{info, warn};
+use log::{info, warn, error};
 use env_logger;
 
 use rdkafka::client::ClientContext;
@@ -16,7 +20,8 @@ use rdkafka::util::get_rdkafka_version;
 
 struct CustomContext {
     offset_storage: String,
-    simulated_split_ref: Arc<Mutex<Vec<String>>>
+    //simulated_split_ref: Arc<Mutex<Vec<String>>>
+    simulated_split_ref: Arc<SplitStore>
 }
 
 impl ClientContext for CustomContext {}
@@ -27,7 +32,7 @@ impl ConsumerContext for CustomContext {
             // Invalidate in progress split. Since partitions may be moving away
             // any in progress state will be invalid and a new split would need
             // to be started from the most recently committed offsets.
-            self.simulated_split_ref.lock().unwrap().clear();
+            self.simulated_split_ref.empty();
 
             let partitions = tpl.elements();
             for entry in partitions.iter() {
@@ -86,14 +91,54 @@ fn commit(consumer: &LoggingConsumer, group_id: &str) -> std::io::Result<()> {
     Ok(())
 }
 
-async fn consume_and_print(brokers: &str, group_id: &str, topics: &[&str]) {
-    let simulated_split = Arc::new(Mutex::new(Vec::new()));
+struct SplitStore {
+    store: Mutex<Vec<String>>,
+    directory: String
+}
+
+impl SplitStore {
+    fn new(directory: &str) -> SplitStore {
+        fs::create_dir_all(directory).expect("Failure creating data output directory");
+
+        SplitStore {
+            store: Mutex::new(Vec::new()),
+            directory: String::from(directory)
+        }
+    }
+
+    fn store(&self, payload: String) {
+        self.store.lock().unwrap().push(payload);
+    }
+
+    fn flush(&self) {
+        let output_file = format!("{}/{}.ldjson", self.directory, chrono::offset::Utc::now().to_rfc3339());
+
+        fs::write(output_file, self.store.lock().unwrap().iter().map( |record| record.to_string() + "\n").collect::<String>())
+            .expect("Unable to save data to file.");
+    }
+
+    fn empty(&self) {
+        self.store.lock().unwrap().clear();
+    }
+
+    fn full(&self) -> bool {
+        self.store.lock().unwrap().len() == 100_000
+    }
+}
+
+async fn process(
+    output_directory: &str, brokers: &str, group_id: &str, topics: &[&str],
+    mut notices: tokio::sync::broadcast::Receiver<&str>,
+    shutdown: tokio::sync::mpsc::Sender<&str>
+) {
+    let simulated_split = Arc::new(SplitStore::new(output_directory));
 
     let context = CustomContext {
         offset_storage: format!("./offsets/{}", group_id),
         simulated_split_ref: simulated_split.clone()
     };
 
+    // Insure directories exist
     // Need to handle all topics
     let path = format!("./offsets/{}/{}/", group_id, topics[0]);
     fs::create_dir_all(path).expect("Failure creating offsets storage directory");
@@ -113,29 +158,43 @@ async fn consume_and_print(brokers: &str, group_id: &str, topics: &[&str]) {
         .expect("Can't subscribe to specified topics");
 
     loop {
-        match consumer.recv().await {
-            Err(e) => warn!("Kafka error: {}", e),
-            Ok(m) => {
-                let payload = match m.payload_view::<str>() {
-                    None => "",
-                    Some(Ok(s)) => s,
-                    Some(Err(e)) => {
-                        warn!("Error while deserializing message payload: {:?}", e);
-                        ""
+        tokio::select! {
+            _ = notices.recv() => {
+                info!("Shutting Down and Flushing on message");
+                simulated_split.flush();
+                commit(&consumer, group_id)
+                    .expect("Error committing offsets");
+
+                break;
+            },
+            record = consumer.recv() => {
+                match record {
+                    Err(e) => warn!("Kafka error: {}", e),
+                    Ok(m) => {
+                        let payload = match m.payload_view::<str>() {
+                            None => "",
+                            Some(Ok(s)) => s,
+                            Some(Err(e)) => {
+                                warn!("Error while deserializing message payload: {:?}", e);
+                                ""
+                            }
+                        };
+
+                        simulated_split.store(payload.to_string());
+
+                        if simulated_split.full() {
+                            simulated_split.flush();
+
+                            commit(&consumer, group_id)
+                                .expect("Error committing offsets");
+
+                            // Simulate starting a new split
+                            simulated_split.empty();
+                        }
                     }
-                };
-
-                simulated_split.lock().unwrap().push(payload.to_string());
-
-                if simulated_split.lock().unwrap().len() == 100_000 {
-                    commit(&consumer, group_id)
-                        .expect("Error committing offsets");
-
-                    // Simulate starting a new split
-                    simulated_split.lock().unwrap().clear();
                 }
             }
-        };
+        }
     }
 }
 
@@ -143,7 +202,7 @@ async fn consume_and_print(brokers: &str, group_id: &str, topics: &[&str]) {
 async fn main() {
     env_logger::init();
 
-    let matches = App::new("consumer example")
+    let matches = App::new("Local Offset Storage Test")
         .version(option_env!("CARGO_PKG_VERSION").unwrap_or(""))
         .about("Experimental locally managed offsets")
         .arg(
@@ -171,15 +230,43 @@ async fn main() {
                 .multiple(true)
                 .required(true),
         )
+        .arg(
+            Arg::with_name("output")
+                .short('o')
+                .long("output")
+                .help("Output directory")
+                .takes_value(true)
+                .multiple(false)
+                .required(true),
+        )
         .get_matches();
 
     info!("Starting");
     let (version_n, version_s) = get_rdkafka_version();
     info!("rd_kafka_version: 0x{:08x}, {}", version_n, version_s);
 
-    let topics = matches.values_of("topics").unwrap().collect::<Vec<&str>>();
-    let brokers = matches.value_of("brokers").unwrap();
-    let group_id = matches.value_of("group-id").unwrap();
+    let (notices_tx, notices_rx) = broadcast::channel(1);
+    let (shutdown_tx, mut shutdown_rx) = mpsc::channel(10);
 
-    consume_and_print(brokers, group_id, &topics).await
+    let shutdown = shutdown_tx.clone();
+    tokio::spawn(async move {
+        let topics = matches.values_of("topics").unwrap().collect::<Vec<&str>>();
+        let brokers = matches.value_of("brokers").unwrap();
+        let group_id = matches.value_of("group-id").unwrap();
+        let output_directory = matches.value_of("output").unwrap();
+
+        process(output_directory, brokers, group_id, &topics, notices_rx, shutdown).await;
+    });
+
+    tokio::select! {
+        _ = signal::ctrl_c() => {
+            info!("Shutting Down");
+            notices_tx.send("Shutdown").unwrap();
+            //drop(notices_tx);
+            info!("Shutdown message sent");
+            drop(shutdown_tx);
+            let _ = shutdown_rx.recv().await;
+            info!("Finalizing Shutdown");
+        }
+    }
 }
